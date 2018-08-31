905008fe781664b6114dda51c50004a86c548061
lauranadolski <lauranadolski@users.noreply.github.com>
2018-08-31 13:27:59 -0400

    Add dummy seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Seed user data
+User.create(first_name: 'Beyonce', last_name: 'Knowles', email_address: 'beyonce@beyonce.com', password_digest: 'lalala')
+User.create(first_name: 'Michelle', last_name: 'Obama', email_address: 'michelle@michelleobama.com', password_digest: 'lalala')
+User.create(first_name: 'Lorra', last_name: 'n/a', email_address: 'laura@laura.com', password_digest: 'lalala')
+User.create(first_name: 'Michelle', last_name: 'Obama', email_address: 'michelle@michelleobama.com', password_digest: 'lalala')
+User.create(first_name: 'Brad', last_name: 'n/a', email_address: 'brad@brad.com', password_digest: 'lalala')
+User.create(first_name: 'Chris', last_name: 'Harrison', email_address: 'chris@thebachelor.com', password_digest: 'lalala')
+
+# Seed intersectionality model data
+IntersectionalityModel.create(name: 'My First Model', note: 'This is my first model, wow-eee how cool', user_id: 1)
+IntersectionalityModel.create(name: 'My First Model', note: 'This is my first model, wow-eee how cool', user_id: 2)
+IntersectionalityModel.create(name: 'My First Model', note: 'This is my first model, wow-eee how cool', user_id: 3)
+IntersectionalityModel.create(name: 'My First Model', note: 'This is my first model, wow-eee how cool', user_id: 4)
+IntersectionalityModel.create(name: 'My First Model', note: 'This is my first model, wow-eee how cool', user_id: 5)
+IntersectionalityModel.create(name: 'My First Model', note: 'This is my first model, wow-eee how cool', user_id: 6)
+
+# Seed categories
+Category.create(name: 'Gender', description: 'Gender is a spectrum.', intersectionality_model_id: 1)
+Category.create(name: 'Gender', description: 'Gender is a spectrum.', intersectionality_model_id: 2)
+Category.create(name: 'Gender', description: 'Gender is a spectrum.', intersectionality_model_id: 3)
+Category.create(name: 'Gender', description: 'Gender is a spectrum.', intersectionality_model_id: 4)
+Category.create(name: 'Gender', description: 'Gender is a spectrum.', intersectionality_model_id: 5)
+Category.create(name: 'Gender', description: 'Gender is a spectrum.', intersectionality_model_id: 6)
