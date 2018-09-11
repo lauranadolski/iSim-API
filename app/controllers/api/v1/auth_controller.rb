76fa931c620c5c750ae90b093234164e828e5247
lauranadolski <lauranadolski@users.noreply.github.com>
2018-09-10 21:30:23 -0400

    More auth work

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -1,12 +1,13 @@
 class Api::V1::AuthController < ApplicationController
   skip_before_action :authorized, only: [:create]
 
+# This is when I post to /api/v1/login
   def create
     @user = User.find_by(email_address: user_login_params[:email_address])
 
     if @user && @user.authenticate(user_login_params[:password])
-      token = encode_token({ user_id: @user.id })
-      render json: { user: UserSerializer.new(@user), jwt: token}, status: :accepted
+      @token = encode_token({ user_id: @user.id })
+      render json: { user: @user, jwt: @token }
     else
       render json: { message: 'Invalid username or password' }, status: :unauthorized
     end
@@ -20,3 +21,6 @@ class Api::V1::AuthController < ApplicationController
   end
 
 end
+
+
+    # render json: { user: UserSerializer.new(@user), jwt: token}, status: :accepted
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,4 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name, :email_address
+  attributes :id, :first_name, :last_name, :email_address, :intersectionality_models
   has_many :intersectionality_models
 end
